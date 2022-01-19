610222dd33494016d739c1013867bd8828bf7f40
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-18 22:54:27 -0600

    test strings

--- sum.test.js
+++ sum.test.js
@@ -57,3 +57,11 @@ test('adding floating point numbers', () => {
     //expect(value).toBe(0.3);           This won't work because of rounding error
     expect(value).toBeCloseTo(0.3); // This works.
 });
+
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+});
