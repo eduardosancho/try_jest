f51957b94744594516d86b62ae363d216cbe6b4d
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-22 20:24:47 -0600

    Add function and test for reverseString function

---
+++ __tests__/reverseString.test.js
@@ -0,0 +1,7 @@
+import reverseString from "../reverseString";
+
+describe('Reverse a string', () => {
+    test('function reverses name eduardo', () => {
+        expect(reverseString('eduardo')).toBe('odraude');
+    });
+});
