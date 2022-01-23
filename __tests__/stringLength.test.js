9e8d6bca23e5ff5b4c3c429252c869e0b7cb6173
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-22 20:19:29 -0600

    Add 'less than 10' test to stringLength.test.js

--- __tests__/stringLength.test.js
+++ __tests__/stringLength.test.js
@@ -2,6 +2,8 @@ import stringLength from "../stringLength";
 
 describe('stringLength related tests', () => {
     test('check if character count in string is in range 1-10', () => {
-        expect(stringLength('eduardo')).toBeGreaterThan(0);
+        const stringSize = stringLength('eduardo');
+        expect(stringSize >= 0).toBeTruthy();
+        expect(stringSize).toBeLessThan(10);
     });
 });
