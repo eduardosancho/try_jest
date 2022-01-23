0c8aaf9db2a5b771cf68aa65addff7008716a5cf
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-22 21:09:00 -0600

    Fix 'not testing .mjs' and add more add() tests

--- __tests__/SimpleCalculator.test.mjs
+++ __tests__/SimpleCalculator.test.mjs
@@ -1,9 +1,21 @@
-import SimpleCalculator from "../SimpleCalculator.mjs";
+import SimpleCalculator from "../SimpleCalculator";
 
-const calculator = new SimpleCalculator();
 
 describe('Tests for the add function', () => {
     test('2 + 2 = 4', () => {
+        const calculator = new SimpleCalculator();
         expect(calculator.add(2, 2)).toBe(4);
     });
+    
+    test('6 + 10 = 16', () => {
+        const calculator = new SimpleCalculator();
+        expect(calculator.add(6, 10)).toEqual(16);
+    });
+
+    test('-5 + -1 = -6', () => {
+        const calculator = new SimpleCalculator();
+        const sum = calculator.add(-5, -1);
+        expect(sum === -6).toBeTruthy();
+    });
+
 });
