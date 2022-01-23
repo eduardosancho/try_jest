1d61d49104037e1a6b03ff8363797356510f9114
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-01-22 20:13:24 -0600

    Fix 'expect to be greater than' typo

--- __tests__/stringLength.test.js
+++ __tests__/stringLength.test.js
@@ -2,6 +2,6 @@ import stringLength from "../stringLength";
 
 describe('stringLength related tests', () => {
     test('check if character count in string is in range 1-10', () => {
-        expect(stringLength('eduardo')).toBeGreaterTHan(0);
+        expect(stringLength('eduardo')).toBeGreaterThan(0);
     });
 });
